1c6340810960814413d3863e2fe8ebb573c04b0a
sohanakhatun <sohanakhatun16@gmail.com>
2024-01-05 01:39:17 +0530

    added more css

--- src/Bookmarks.jsx
+++ src/Bookmarks.jsx
@@ -1,20 +1,23 @@
-import React, { useEffect, useState } from "react";
-import { connect, useSelector } from "react-redux";
+import React from "react";
+import { connect } from "react-redux";
 import { removeBookmarkAction } from "./redux/BookmarkActions";
-import toast from "react-hot-toast";
 
 const Bookmarks = ({ bookmarks, removeBookmarkAction }) => {
-
   return (
     <div>
-      <h2 className="text-white font-Poppins font-semibold text-center mb-4">Bookmarks</h2>
+      <h2 className="text-white font-Poppins font-semibold text-center mb-4">
+        Bookmarks
+      </h2>
       <div className="flex flex-col gap-4">
         {bookmarks.map((bookmark) => (
-          <div  className={
-          bookmarks.length > 0
-            ? "bg-[#D05252] w-fit rounded-[10px] p-4  justify-center items-center mx-auto text-center text-xl font-bold text-white"
-            : "hidden"
-        } key={bookmark.id}>
+          <div
+            className={
+              bookmarks.length > 0
+                ? "bg-[#D05252] w-fit rounded-[10px] p-4  justify-center items-center mx-auto text-center text-xl font-bold text-white"
+                : "hidden"
+            }
+            key={bookmark.id}
+          >
             <p> {bookmark.content}</p>
             <p className="text-center">- {bookmark.author}</p>
           </div>
--- src/redux/reducers/BookmarkReducers.js
+++ src/redux/reducers/BookmarkReducers.js
@@ -1,26 +1,28 @@
 const initialState = {
-    bookmarks: [],
-  };
-  // Reducer for adding bookmarks
-  export const addBookmarksReducer = (state = initialState , action) => {
-    switch (action.type) {
-      case "ADD_BOOKMARK":
-        return { ...state, bookmarks: [...state.bookmarks, action.payload] };
-  
-      default:
-        return state;
-    }
-  };
-  
-  export const removeBookmarksReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case "REMOVE_BOOKMARK":
-        return {
-          ...state,
-          bookmarks: state.bookmarks.filter((bookmark) => bookmark.id !== action.payload),
-        };
-  
-      default:
-        return state;
-    }
-  };
+  bookmarks: [],
+};
+// Reducer for adding bookmarks
+export const addBookmarksReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "ADD_BOOKMARK":
+      return { ...state, bookmarks: [...state.bookmarks, action.payload] };
+
+    default:
+      return state;
+  }
+};
+
+export const removeBookmarksReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "REMOVE_BOOKMARK":
+      return {
+        ...state,
+        bookmarks: state.bookmarks.filter(
+          (bookmark) => bookmark.id !== action.payload
+        ),
+      };
+
+    default:
+      return state;
+  }
+};
